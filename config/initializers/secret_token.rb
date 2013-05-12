bec8b8f2993057dd7936dfbaeb256b143bdf1af4
Ryan Smith <galaxyhikersinc@gmail.com>
2013-05-11 23:17:37 -0700

    Moved secret token to application.yml

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,5 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Otwohnet::Application.config.secret_token = '0714872f31da6f8e6bc16a3158b0c63cb9d2dab0f532c3b93a7fd3142a3edc2225362deaeea91ff9637aeec2c6e7ae438e9d5a64da26551c97d9a0c477a04489'
+Otwohnet::Application.config.secret_token = ENV['SECRET_TOKEN'] || 'fallback_token_for_development'
+
