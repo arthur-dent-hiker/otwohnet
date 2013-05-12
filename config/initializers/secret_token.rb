93700efe11f73767774729264fbe9c139d57b2fc
Ryan Smith <galaxyhikersinc@gmail.com>
2013-05-11 20:47:02 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Otwohnet::Application.config.secret_token = '0714872f31da6f8e6bc16a3158b0c63cb9d2dab0f532c3b93a7fd3142a3edc2225362deaeea91ff9637aeec2c6e7ae438e9d5a64da26551c97d9a0c477a04489'
