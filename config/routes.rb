6c98bbb5a8465df1f55266fe368e14b3257d5454
Ryan Smith <galaxyhikersinc@gmail.com>
2013-05-15 10:21:00 -0700

    Changed connection route to avoid conflict

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Otwohnet::Application.routes.draw do
   resources :categories
   resources :connection_invitations
   match 'connection_invitations/make' => "connection_invitations#make", :via=>:post, :as=>"new_connection_invitations"
-  match '/:id' => "connections#new", :via=>:get
+  match '/connect/:id' => "connections#new", :via=>:get
   authenticated :user do
     root :to => 'home#index'
   end
